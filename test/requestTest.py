072f69a95024cca69f8f8ce00b526ddf740a0153
Royg <royg@jfrog.com>
2014-07-06 16:36:03 +0300

    Made test more generic

--- test/requestTest.py
+++ test/requestTest.py
@@ -3,30 +3,53 @@ import sys
 import requests
 
 
+############################################################
+############        Global Variables         ###############
+############################################################
+URL        = "https://api.trello.com/1/"
+BOARD      = "boards/i8AxuVZG/cards"
+USER_KEY   = "b0bb19aa5adaa321f38ea1fee886c30f"
+TOKKEN_API = "5e41f24ad3e47dbf8d2ed3a9610ab923f73e1ce21f294afc4ddb333ae2ca087e"
+
+
+
+############################################################
+############       Helper Methods          #################
+############################################################
 def prettyPrint(message, token ='*'):
     print len(message)* token
     print message
     print len(message)* token
+##--------------------------------------------------------##
+def genericRequest(url, payload):
+    r = requests.get(url, params=payload)
+    for oneResult in r.json():
+        print oneResult
 
-#-----------------
 
-if __name__ == "__main__":
-    rc = 0
-    url = "https://api.trello.com/1/boards/i8AxuVZG/cards"
+
+############################################################
+############        Main Methods           #################
+############################################################
+def getNameRequest():
+    url = "%s%s" % (URL,BOARD)
     payload = {
-        'fields': 'name',
-        'key': 'b0bb19aa5adaa321f38ea1fee886c30f',
-        'token': '5e41f24ad3e47dbf8d2ed3a9610ab923f73e1ce21f294afc4ddb333ae2ca087e',
-        }
-    try:
-        r = requests.get(url, params=payload)
-        for oneResult in r.json():
-            print oneResult
-        prettyPrint("Done printing all requests!")
+                'fields': 'name',
+                'key': USER_KEY,
+                'token': TOKKEN_API,
+              }
+    genericRequest(url,payload)
 
+
+##---------------------------------------##
+if __name__ == "__main__":
+    rc = -1
+    try:
+        getNameRequest()
+        prettyPrint("Done printing all requests!")
+        rc = 0
     except  ValueError as error:
         print "ValueError caught with error: %s" % error
-        rc = -1
     finally:
         sys.exit(rc)
 

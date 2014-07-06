4b0dfa7c94d971e0f49d69b9f2040c6de355cf10
yluft <yluft@jfrog.com>
2014-07-06 16:16:22 +0300

    Added support for reading appKey and userId from configuration file

---
+++ src/gamello/AppPermissions.py
@@ -0,0 +1,29 @@
+from ConfigParser import ConfigParser
+
+__author__ = 'yluft'
+
+
+class AppPermissions:
+
+    def __init__(self, **keys):
+        if keys['file']:
+            self.parseConfig(keys['file'])
+        else:
+            raise NotImplementedError('Cannot initialize without file')
+
+    def parseConfig(self, fileName):
+        configParser = ConfigParser()
+        configParser.read(fileName)
+        self.appKey = configParser.get('Permissions', 'appKey')
+        self.userId = configParser.get('Permissions', 'userId')
+
+
+# Test
+def testReading():
+    permissions = AppPermissions(file='gamello.test.config')
+    assert permissions.appKey == 'TestAppKey'
+    assert permissions.userId == 'userId'
+
+
+if __name__ == '__main__':
+    testReading()
---
+++ src/gamello/BoardInitializer.py
@@ -0,0 +1,4 @@
+__author__ = 'yluft'
+
+if __name__ == '__main__':
+    pass
